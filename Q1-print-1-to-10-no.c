198f7b5b71bcc3576d1a984524c8658949914e82
SakshiRudrawar <sakshirudrawar04@gmail.com>
2024-06-11 12:09:22 +0530

    Add files via upload

---
+++ Q1-print-1-to-10-no.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+	int a=0;
+	
+	while(a<10)
+	{
+		a++;
+		printf("\n %d",a);
+	}
+}
+
+
+#include<stdio.h>
+void main()
+{
+	int i;
+	
+	for(i=0;i<10;i++)
+	{
+		printf("\n %d, a");
+	}
+}
