02ec6a78ba1b0dfade13ae2f7cf992e409d59f98
SakshiRudrawar <sakshirudrawar04@gmail.com>
2024-06-11 12:41:03 +0530

    Add files via upload

---
+++ A7-Q6-print-only-prime-no-array.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void main() {
+    int i,j, factors = 0;
+    int arr[7];
+
+    for (i = 0; i < 7; i++) {
+        printf("Enter a number: ");
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < 7; i++) 
+	{
+         factors = 0; 
+
+        if (arr[i] <= 1) 
+		{
+            factors = 1; 
+        } 
+		else 
+		{
+            for (j = 2; j * j <= arr[i]; j++) 
+			{
+                if (arr[i] % j == 0) 
+				{
+                    factors++;
+                    break;
+                }
+            }
+        }
+
+        if (factors == 0) 
+		{
+            printf("%d is a prime number\n", arr[i]);
+        } 
+    }
+}
+
+
