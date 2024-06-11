d528df6daf933440090ad2b1ec7c54ec82ec0b81
SakshiRudrawar <sakshirudrawar04@gmail.com>
2024-06-11 12:24:03 +0530

    Add files via upload

---
+++ Q7-factorial-using-while.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int i=1,num;
+	long long factorial=1;
+	printf("enter rhe number");
+	scanf("%d",&num);
+	
+	while(i<=num)
+	{
+		factorial *= i;
+		i++;
+		
+	}
+	
+	printf("the factorial of a no : %d" , factorial);
+}
