0c39e2fa754e6cfa707673dfc249d9b8d42bb061
SakshiRudrawar <sakshirudrawar04@gmail.com>
2024-06-11 13:08:12 +0530

    Add files via upload

---
+++ A12-Q9-revese-array-using-malloc.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	int i,temp;
+	int n=5;
+	int* arr;
+	
+	arr = (int*)malloc(sizeof(int)*n);
+	
+	for(i=0;i<n;i++)
+	{
+		printf("enter the number");
+		scanf("%d",&arr[i]);
+	}
+	
+	for(i=0;i<2;i++)
+	{
+		temp=arr[i];
+		arr[i]=arr[4-i];
+		arr[4-i]=temp;
+	}
+	
+	printf("Reversed array");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t",arr[i]);
+	}
+}
