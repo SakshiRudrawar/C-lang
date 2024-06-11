27472c38fd93254091de7b03c45acfd79d45f3b7
SakshiRudrawar <sakshirudrawar04@gmail.com>
2024-06-11 12:23:10 +0530

    Add files via upload

---
+++ Q7-factorial-using-for.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int i, num;
+	long long factorial=1;
+	
+	printf("enter the number");
+	scanf("%d", &num);
+	
+	for(i=1; i<=num; i++)
+	{
+		factorial *= i;	// factorial = factorial * i
+	}
+	
+	printf("factorial of the given num is %d", factorial);
+	
+}
