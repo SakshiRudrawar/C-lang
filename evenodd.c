0b46ce568925a92fb4ff8bec7b6f5433ae313fe8
SakshiRudrawar <sakshirudrawar04@gmail.com>
2024-06-11 12:34:21 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int evenodd(int*,int*,int*);
+void main()
+{
+	int number,j,k;
+	printf("Enter the number:",number);
+	scanf("%d",&number);
+	evenodd(&number,&j,&k);
+	if(j==number)
+	{
+		printf("%d Is a even number",j);
+	}
+	if(k==number)
+	{
+		printf("%d Is a odd number",k);
+	}
+}
+int evenodd(int* number,int*j,int*k)
+{
+	
+	if(*number%2==0)
+	{
+	*j=*number;//	printf("%d is a even number",number);
+	}
+	else
+	{
+	*k=*number;//	printf("%d is a odd number",number);
+	}
+}
