c8e1b89471c45a0c41b90090aaa2a8c5064a99e0
SakshiRudrawar <sakshirudrawar04@gmail.com>
2024-06-11 12:24:25 +0530

    Add files via upload

---
+++ Q8-strong-no-using-while.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void main() 
+{
+    int number, remainder, factorial, sum = 0, temp, i;
+
+    printf("Enter the number: ");
+    scanf("%d", &number);
+    temp = number;
+
+    while (number > 0) {
+        remainder = number % 10;
+        factorial = 1;
+
+        for (i = 1; i <= remainder; i++) {
+            factorial *= i;
+        }
+
+        sum += factorial;
+        number /= 10;
+    }
+
+    if (sum == temp) 
+	{
+        printf("%d is a strong number\n", temp);
+    } 
+	else
+	{
+        printf("%d is not a strong number\n", temp);
+    }
+
+}
+
