4b922c946e6a4feaad34c0589f3824e179602083
SakshiRudrawar <sakshirudrawar04@gmail.com>
2024-06-11 12:48:08 +0530

    Add files via upload

---
+++ A9-Q4-strlen.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char str[5]={'a','b','c','d','e'};
+	
+	int length = strlen(str);
+	
+	printf("string is %s\n",str);
+	
+	printf("length of a string is : %d\n",length);
+	
+	return 0;
+}
