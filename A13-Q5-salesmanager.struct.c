058374bc9418bf3b7c1ec7d4f03c8b2fead10c0d
SakshiRudrawar <sakshirudrawar04@gmail.com>
2024-06-11 13:10:40 +0530

    Add files via upload

---
+++ A13-Q5-salesmanager.struct.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<string.h>
+
+struct salesManager
+{
+	int id;
+	char name[20];
+	double salary;
+	double incentive;
+	char target[70];
+};
+
+void main()
+{
+	int i;
+	
+	struct salesManager arr[3];
+	
+	for(i=0; i<3; i++)
+	{
+		printf("enter the id");
+		scanf("%d", &arr[i].id);
+		
+		printf("enter the name");
+		scanf("%s", &arr[i].name);
+		
+		printf("enter the salary");
+		scanf("%lf", &arr[i].salary);
+		
+		printf("enter the incentive");
+		scanf("%lf", &arr[i].incentive);
+		
+		printf("enter the target");
+		scanf("%s",&arr[i].target);
+		
+		
+	}
+	
+	for(i=0;i<3;i++)
+	{
+		printf("salesman id is :%d\n  name: %s\n  salary:%lf\n  allowance:%lf\n  target:%s\n" , arr[i].id, arr[i].name, arr[i].salary, arr[i].incentive, arr[i].target );
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
