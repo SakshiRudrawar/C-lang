c93ff612ce255e46e548e1aac46a9ffff8de0bd1
SakshiRudrawar <sakshirudrawar04@gmail.com>
2024-06-11 12:32:13 +0530

    Add files via upload

---
+++ 3 digit sum&rev.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int add(int*,int*,int*,int*,int*);
+void main()
+{
+	int num1,num2,num3,sum,rev;
+	printf("Enter the num1:",num1);
+	scanf("%d",&num1);
+	printf("Enter the num2:",num2);
+	scanf("%d",&num2);
+	printf("Enter the num3:",num3);
+	scanf("%d",&num3);
+	add(&num1,&num2,&num3,&rev,&sum);
+	printf("The revers number is: %d\n The sum of number is: %d",rev,sum);
+}
+int add(int*a,int*b,int*c,int*rev,int*sum)
+{
+	
+	*rev=*c*100+*b*10+*a;
+    *sum=*a+*b+*c;
+	
+}
