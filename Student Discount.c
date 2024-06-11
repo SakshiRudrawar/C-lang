5a936ca020d0084b8fec12fed9e98237567ef8a1
SakshiRudrawar <sakshirudrawar04@gmail.com>
2024-06-11 12:35:26 +0530

    Add files via upload

---
+++ Student Discount.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+int discount(int*,int*,char*,int*,int*,int*,int*);
+void main()
+{
+	int price,total,a,b,c,d;
+	char ans;
+	printf("Enter the price:");
+	scanf("%d",&price);
+	fflush(stdin);
+	printf("Are you a student(y/n):",ans);
+	scanf("%c",&ans);
+	discount(&price,&total,&ans,&a,&b,&c,&d);
+	if(a==total)
+	{
+	printf("%d is the total after 20 discount",a);	
+	}
+	if(b==total)
+	{
+	printf("%d is the total after 10 discount",b);	
+	}
+	if(total==c)
+	{
+	printf("%d is the total after 15 discount",c);	
+	}
+	if(total==d)
+	{
+	printf("%d is the total after 0 discount",d);	
+	}
+}
+int discount(int*price,int*total,char*ans,int*a,int*b,int*c,int*d)
+{
+	if(*ans=='y')
+	{
+		if(*price>=500)
+		{
+			*a=*total=*price-(*price*20/100);	
+		//	printf("%d is the total after 20% discount",total);
+		}
+		else
+		{
+			*b=*total=*price-(*price*10/100);
+		//	printf("%d is the total after 10% discount",total);
+		}
+	}
+	if(*ans=='n')
+	{
+		if(*price>=600)
+		{
+			*c=*total=*price-(*price*15/100);
+		
+		//	printf("%d is the total after 15% discount",total);
+		}
+		else
+		{
+			*d=*total=*price;
+			
+		//	printf("%d id the total after 0% discount",total);
+		}
+	}
+}
